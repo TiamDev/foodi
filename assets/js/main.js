53d736ff6e9382ad0a99e96394a8d16a7cf8a855
TiamDev <fatimabakar707@gmail.com>
2023-10-16 08:58:50 +0300

    use hover , slider update and js events

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,29 @@
 
 let box = document.getElementsByClassName("ox");
+let boxSize=box[0].offsetWidth+50;
 let inner = document.getElementsByClassName("inner")[0];
 let count = 0;
 function next() {
     let boxNum = box.length;
-    console.log(boxNum + " boxs");
     if (count < boxNum - 1) {
         count++;
-        console.log(boxNum - 1 + "---" + count);
-        inner.style.transform = `translateX(${-370 * count}px)`;
+        inner.style.transform = `translateX(${-boxSize * count}px)`;
     }
-
-
 }
 function pre() {
     let boxNum = box.length;
     if (count < boxNum && count != 0) {
         --count;
-        inner.style.transform = `translateX(-${370 * count}px)`;
+        inner.style.transform = `translateX(-${boxSize * count}px)`;
+    }
+}
+function changeIcon(icon){
+    console.log(icon);
+    if(icon.classList.contains('fa-regular')===true){
+        icon.classList.remove('fa-regular');
+        icon.classList.add('fa');
+    }
+    else{
+        icon.classList.add('fa-regular');
     }
 }
